dcf1b6a09e497c9c3e08caeac7071a92361e109e
VeryrrDefine <1308061599@qq.com>
2026-02-19 23:18:21 +0800

    bx

--- framework.js
+++ framework.js
@@ -245,10 +245,17 @@ register.forEach((notation, index) => {
 					h = h.concat(e);
 				}
 			},
+			showExpand(expr) {
+				try {
+					return this.able(expr);
+				} catch {
+					return false;
+				}
+			},
 		},
 		template:
 			`<li><div class="shown-item" @mouseenter="recalculate" @mouseleave="unshow()">
-            <button class="a1" @mousedown="expand">Expand</button>
+            <button class="a1" @mousedown="expand" v-if="showExpand(expr)">Expand</button>
             <button class="a2" @mousedown="change_anal(expr)">Change analysis</button><span v-html="a(display(expr),anal)"></span>
             <div class="anal">
             <span></div>
